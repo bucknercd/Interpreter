cf99153b0ed6efca75cd37b479532cccfad0b154
Chris Buckner <christopher.d.buckner@gmail.com>
2019-09-26 23:03:03 -0500

    added an error message to main HavaBol file if an input filename is not provided. created a bash script to update the system havabol bin files installed on the system

--- src/havabol/HavaBol.java
+++ src/havabol/HavaBol.java
@@ -2,6 +2,10 @@ package havabol;
 
 public class HavaBol {
     public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Please provide a .hav program for the HavaBol interpreter.");
+            System.exit(1);
+        }
         try {
           SymbolTable st = new SymbolTable();
           Parser parser = new Parser(args[0], st);
---
+++ update_havabol_system_bin.sh
@@ -0,0 +1,4 @@
+# This updates the system binary files for havabol on this machine
+#!/usr/bin/bash
+
+sudo cp -r bin/havabol /etc/Interpreter/bin
